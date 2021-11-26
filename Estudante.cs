6b53fc7126272b870da01b94f92686598fb599d7
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-25 22:03:19 -0300

    Adicionar arquivos de projeto.

---
+++ Estudante.cs
@@ -0,0 +1,20 @@
+﻿namespace AppVetorExercicio03
+{
+    class Estudante
+    {
+        public string nomeEstudante { get; set; }
+
+        public string emailEstudante { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            this.nomeEstudante = nome;
+            this.emailEstudante = email;
+        }
+
+        public override string ToString()
+        {
+            return nomeEstudante +", "+emailEstudante;
+        }
+    }
+}
